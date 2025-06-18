680f0779925fc2afef6e811a7bf3689d8b948c6e
Yehouda Arnauve <yehouda_arnauve@hotmail.com>
2025-06-18 12:04:39 +0200

    add constraint for professional client

--- src/main/java/com/exercice/model/ClientProfessional.java
+++ src/main/java/com/exercice/model/ClientProfessional.java
@@ -1,6 +1,7 @@
 package com.exercice.model;
 
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,14 @@ import java.util.List;
 @NoArgsConstructor(force = true)
 public class ClientProfessional implements IClient {
     public static final long TEN_MILLIONS_ANNUAL_TURNOVER = 10_000_000L;
+    @NotNull
     String id;
+    @NotNull
     String companyName;
     String tva;
+    @NotNull
     String siren;
+    @NotNull
     Long annualTurnover; // à voir pour un BigDecimal
     @Builder.Default
     List<Product> shoppingCart = new ArrayList<>();
